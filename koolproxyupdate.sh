835bd399bed7e8f32566f514fc71f9dbbd6fb1d8
YujiaCheng1996 <cyj@programmer.net>
2020-10-05 12:55:05 +0800

    Restore to GNU Sed command

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -8,5 +8,5 @@ cp files/usr/share/koolproxy/data/user.txt files/usr/share/koolproxy/data/rules/
 wget 'https://cdn.jsdelivr.net/gh/user1121114685/koolproxyR/koolproxyR/koolproxyR/data/koolproxyR_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
 
 wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '' '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '' '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
+sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
+sed -i '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
