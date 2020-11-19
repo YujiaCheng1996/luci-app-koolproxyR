998170379e9b86fa7f7cf5cfdc2a0a81a429d44a
YujiaCheng1996 <cyj@programmer.net>
2020-11-19 14:05:29 +0800

    update rules

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,12 +1,9 @@
-wget 'https://cdn.jsdelivr.net/gh/houzi-/CDN/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
 wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/yhosts.txt' -O files/usr/share/koolproxy/data/rules/yhosts.txt
 wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/fanboy.txt' -O files/usr/share/koolproxy/data/rules/fanboy.txt
 wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/easylistchina.txt' -O files/usr/share/koolproxy/data/rules/easylistchina.txt
-
+wget 'https://cdn.jsdelivr.net/gh/houzi-/CDN/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
 wget 'https://cdn.jsdelivr.net/gh/houzi-/CDN/user.txt' -O files/usr/share/koolproxy/data/user.txt
 cp files/usr/share/koolproxy/data/user.txt files/usr/share/koolproxy/data/rules/user.txt
-wget 'https://cdn.jsdelivr.net/gh/user1121114685/koolproxyR/koolproxyR/koolproxyR/data/koolproxyR_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
 
-wget https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt -O- | grep ^\|\|[^\*]*\^$ | sed -e 's:||:address\=\/:' -e 's:\^:/0\.0\.0\.0:' > files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '/youku/d' files/usr/share/koolproxy/dnsmasq.adblock
-sed -i '/[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}\.[1-9]\{1,3\}/d' files/usr/share/koolproxy/dnsmasq.adblock
+wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/koolproxy_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
+wget 'https://cdn.jsdelivr.net/gh/privacy-protection-tools/anti-AD/adblock-for-dnsmasq.conf' -O files/usr/share/koolproxy/dnsmasq.adblock
