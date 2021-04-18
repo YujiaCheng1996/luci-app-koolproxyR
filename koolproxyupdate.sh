013eac358de3c9ba1fa82fff7a616932554b338e
YujiaCheng1996 <cyj@programmer.net>
2021-04-18 18:50:22 +0800

    Update rules CDN address

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,10 +1,14 @@
-wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/yhosts.txt' -O files/usr/share/koolproxy/data/rules/yhosts.txt
-wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/fanboy.txt' -O files/usr/share/koolproxy/data/rules/fanboy.txt
-wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/easylistchina.txt' -O files/usr/share/koolproxy/data/rules/easylistchina.txt
-wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/AdGuardHome.txt' -O files/usr/share/koolproxy/data/rules/AdGuardHome.txt
-wget 'https://cdn.jsdelivr.net/gh/houzi-/CDN/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
-wget 'https://cdn.jsdelivr.net/gh/houzi-/CDN/user.txt' -O files/usr/share/koolproxy/data/user.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/kp.dat' -q -O files/usr/share/koolproxy/data/rules/kp.dat
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/daily.txt' -q -O files/usr/share/koolproxy/data/rules/daily.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/koolproxy.txt' -q -O files/usr/share/koolproxy/data/rules/koolproxy.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/yhosts.txt' -q -O files/usr/share/koolproxy/data/rules/yhosts.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/fanboy.txt' -q -O files/usr/share/koolproxy/data/rules/fanboy.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/easylistchina.txt' -q -O files/usr/share/koolproxy/data/rules/easylistchina.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/antiad.txt' -q -O files/usr/share/koolproxy/data/rules/anti-ad.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/mv.txt' -q -O files/usr/share/koolproxy/data/rules/mv.txt
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/rules/kpr_our_rule.txt' -q -O files/usr/share/koolproxy/data/user.txt
+
 cp files/usr/share/koolproxy/data/user.txt files/usr/share/koolproxy/data/rules/user.txt
 
-wget 'https://cdn.jsdelivr.net/gh/ycg31/KoolProxy/rules/koolproxy_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
-wget 'https://cdn.jsdelivr.net/gh/neodevpro/neodevhost@master/host_dnsmasq.conf' -O files/usr/share/koolproxy/dnsmasq.adblock
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/ipsetadblock/koolproxy_ipset.conf' -q -O files/usr/share/koolproxy/koolproxy_ipset.conf
+wget 'https://cdn.jsdelivr.net/gh/project-lede/koolproxy@main/ipsetadblock/dnsmasq.adblock' -q -O files/usr/share/koolproxy/dnsmasq.adblock
